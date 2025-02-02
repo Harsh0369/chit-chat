e2a8017707a9b75e312ff32088caca97a82f21df
Harsh <harshagarwal81528@gmail.com>
2025-02-02 18:03:07 +0530

    signup route and functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,43 @@
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+
 export const signup = async (req, res) => {
-    res.send('signup');
+    const {email,password,fullname} = req.body;
+    try {
+        if(!email || !password || !fullname){
+            return res.status(400).send("Please fill in all fields");
+        }
+        if (password.length < 6) {
+            return res.status(400).send("Password must be at least 6 characters long");
+        }
+
+        const user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).send("User with this email already exists");
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            email,
+            password: hashedPassword,
+            fullname,
+        });
+
+        if (newUser) {
+            generateToken(newUser._id, res);
+            await newUser.save();
+
+            res.status(201).json({message:"User created successfully"});
+        }
+        else {
+            res.status(400).send("Invalid user data");
+        }
+    }
+    catch (err) {
+        console.log(err);
+    }
 }
 
 export const login = async (req, res) => {
--- backend/src/index.js
+++ backend/src/index.js
@@ -8,10 +8,13 @@ dotenv.config();
 
 
 const app = express();
+app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 
-app.use('/api/auth',authRoutes);
+app.use('/api/auth', authRoutes);
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
